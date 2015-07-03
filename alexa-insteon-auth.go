ca66b4a33367a03fc7eaf31dfa606fbf47d7eaac
Swiss <chrisp.schafer@gmail.com>
2015-07-02 19:17:37 -0700

    added url shortening, switched to a switch statement from if elses

--- alexa-insteon-auth.go
+++ alexa-insteon-auth.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	//      "io"
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"github.com/swisskid/go-insteon/insteon"
 	"io/ioutil"
+	"log"
 	"net/http"
 	//"net/url"
 	//"strings"
+	"./secrets"
 	"strconv"
 )
+
 var Accounts map[string]Acc_Info
 var AccDevs map[string][]insteon.Device
 var AccScenes map[string][]insteon.Scene
@@ -24,7 +28,6 @@ func main() {
 	http.HandleFunc("/", foo)
 	http.ListenAndServe(":9003", nil)
 }
-
 
 type Acc_Info struct {
 	Amazon       string
@@ -107,6 +110,8 @@ func foo(w http.ResponseWriter, r *http.Request) {
 	j.Version = "1.0"
 	k.Type = "PlainText"
 	j.Response.ShouldEndSession = true
+	userid := l.Session.User.UserId
+	client_id := insteon.Client_Id
 	if l.Request.Intent.Name != "" {
 		fmt.Println("The name of the intent is " + l.Request.Intent.Name)
 		fmt.Println(l.Request.Intent.Slots)
@@ -115,100 +120,99 @@ func foo(w http.ResponseWriter, r *http.Request) {
 			fmt.Println(slot.Value)
 		}
 	}
-	userid := l.Session.User.UserId
-	client_id := insteon.Client_Id
-	if l.Request.Intent.Name == "Register" {
+	switch l.Request.Intent.Name {
+	case "Register":
 		var m Card
-		url := "http://connect.insteon.com/api/v2/oauth2/auth?client_id=" + client_id + "&state=" + userid + "&response_type=code&redirect_uri=http://veryoblivio.us:9001/"
+		long_url := "http://connect.insteon.com/api/v2/oauth2/auth?client_id=" + client_id + "&state=" + userid + "&response_type=code&redirect_uri=http://veryoblivio.us:9001/"
+		short_url := ShortenURL(long_url)
 		m.Type = "Simple"
 		m.Title = "Register your Insteon Connection"
-		m.Content = url
+		m.Content = short_url
 		j.Response.Card = &m
 		k.Text = "Please follow the link on the card to sign into your insteon account"
-	}
-	fmt.Println(Accounts)
-	fmt.Println("PreIF")
-	if val, ok := Accounts[userid]; ok {
-		fmt.Println("I'm going into the IF")
-		insteon.Access_Token = val.Access_Token
-		insteon.DevList = AccDevs[userid]
-		insteon.SceneList = AccScenes[userid]
-		insteon.RoomList = AccRooms[userid]
-		fmt.Println("Post assignment")
-		fmt.Println(insteon.Access_Token)
-	} else {
-		fmt.Println("I'm going into the ELSE")
-		if val2, er2 := GetAccInfo(userid); er2 { //er2 is actually success if false - like an error
-			fmt.Println("I'm going into the ELSEELSE")
-			var m Card
-			client_id := insteon.Client_Id
-			url := "http://connect.insteon.com/api/v2/oauth2/auth?client_id=" + client_id + "&state=" + userid + "&response_type=code&redirect_uri=http://veryoblivio.us:9001/"
-			m.Type = "Simple"
-			m.Title = "Register your Insteon Connection"
-			m.Content = url
-			j.Response.Card = &m
-			k.Text = "No linked account found. Please follow the link on the card to sign into your insteon account"
-		} else {
-			fmt.Println("I'm going into the ELSEIF")
-			insteon.Access_Token = val2.Access_Token
-			insteon.PopulateAll()
-			AccDevs[userid] = insteon.DevList
-			AccScenes[userid] = insteon.SceneList
-			AccRooms[userid] = insteon.RoomList
-			Accounts[userid] = val2
+		break
+	case "Lighting", "Activate":
+		fmt.Println(Accounts)
+		fmt.Println("PreIF")
+		if val, ok := Accounts[userid]; ok {
+			fmt.Println("I'm going into the IF")
+			insteon.Access_Token = val.Access_Token
+			insteon.DevList = AccDevs[userid]
+			insteon.SceneList = AccScenes[userid]
+			insteon.RoomList = AccRooms[userid]
 			fmt.Println("Post assignment")
 			fmt.Println(insteon.Access_Token)
-			fmt.Println(Accounts)
-		}
-	}
-
-	if l.Request.Intent.Name == "Lighting" { //Engage insteon subroutine
-		var direction string
-		var item string
-		var n insteon.Command
-		for _, slot := range l.Request.Intent.Slots {
-			if slot.Name == "Direction" {
-				direction = slot.Value
-				continue
-			}
-			if slot.Name == "Device" {
-				item = slot.Value
-			}
-		}
-		n.Command = direction
-		item_type, id, location := insteon.SearchString(item)
-		fmt.Println(item_type + " ID = " + strconv.Itoa(id) + " Location " + strconv.Itoa(location))
-		switch item_type {
-		case "device":
-			n.Device_Id = id
-			n.Level = insteon.DevList[location].DimLevel / 254 * 100
-			k.Text = "Turning " + item + " " + direction
-		case "scene":
-			n.Scene_Id = id
-			k.Text = "Turning " + item + " " + direction
-		case "room":
-			fmt.Println("Rooms are a pain")
-			k.Text = "Rooms are a pain, do it yourself"
-		case "not_found":
-			k.Text = "The following device, scene, or room was not found: " + item
-		}
-		insteon.RunCommand(n)
-	} else {
-
-		k.Text = "I am a working response"
-	}
-	if l.Request.Intent.Name == "Activate" { //Activate a scene
-		fmt.Println(insteon.Access_Token + " is the accesss token")
-		var n insteon.Command
-		n.Command = "on"
-		scene_name := l.Request.Intent.Slots["Scene"].Value
-		ret_type, scene_id, _ := insteon.SearchString(scene_name)
-		if ret_type == "scene" {
-			n.Scene_Id = scene_id
-			insteon.RunCommand(n)
-			k.Text = "Turning the scene " + scene_name + " on"
 		} else {
-			k.Text = "Failed to find scene " + scene_name
+			fmt.Println("I'm going into the ELSE")
+			if val2, er2 := GetAccInfo(userid); er2 { //er2 is actually success if false - like an error
+				fmt.Println("I'm going into the ELSEELSE")
+				var m Card
+				client_id := insteon.Client_Id
+				long_url := "http://connect.insteon.com/api/v2/oauth2/auth?client_id=" + client_id + "&state=" + userid + "&response_type=code&redirect_uri=http://veryoblivio.us:9001/"
+				short_url := ShortenURL(long_url)
+				m.Type = "Simple"
+				m.Title = "Register your Insteon Connection"
+				m.Content = short_url
+				j.Response.Card = &m
+				k.Text = "No linked account found. Please follow the link on the card to sign into your insteon account"
+			} else {
+				fmt.Println("I'm going into the ELSEIF")
+				insteon.Access_Token = val2.Access_Token
+				insteon.PopulateAll()
+				AccDevs[userid] = insteon.DevList
+				AccScenes[userid] = insteon.SceneList
+				AccRooms[userid] = insteon.RoomList
+				Accounts[userid] = val2
+				fmt.Println("Post assignment")
+				fmt.Println(insteon.Access_Token)
+				fmt.Println(Accounts)
+			}
+		}
+		switch l.Request.Intent.Name {
+		case "Lighting":
+			var direction string
+			var item string
+			var n insteon.Command
+			for _, slot := range l.Request.Intent.Slots {
+				if slot.Name == "Direction" {
+					direction = slot.Value
+					continue
+				}
+				if slot.Name == "Device" {
+					item = slot.Value
+				}
+			}
+			n.Command = direction
+			item_type, id, location := insteon.SearchString(item)
+			fmt.Println(item_type + " ID = " + strconv.Itoa(id) + " Location " + strconv.Itoa(location))
+			switch item_type {
+			case "device":
+				n.Device_Id = id
+				n.Level = insteon.DevList[location].DimLevel / 254 * 100
+				k.Text = "Turning " + item + " " + direction
+			case "scene":
+				n.Scene_Id = id
+				k.Text = "Turning " + item + " " + direction
+			case "room":
+				fmt.Println("Rooms are a pain")
+				k.Text = "Rooms are a pain, do it yourself"
+			case "not_found":
+				k.Text = "The following device, scene, or room was not found: " + item
+			}
+			insteon.RunCommand(n)
+		case "Activate":
+			fmt.Println(insteon.Access_Token + " is the accesss token")
+			var n insteon.Command
+			n.Command = "on"
+			scene_name := l.Request.Intent.Slots["Scene"].Value
+			ret_type, scene_id, _ := insteon.SearchString(scene_name)
+			if ret_type == "scene" {
+				n.Scene_Id = scene_id
+				insteon.RunCommand(n)
+				k.Text = "Turning the scene " + scene_name + " on"
+			} else {
+				k.Text = "Failed to find scene " + scene_name
+			}
 		}
 	}
 	j.Response.OutputSpeech = &k
@@ -230,9 +234,34 @@ func GetAccInfo(amazon_id string) (account_info Acc_Info, success bool) {
 	account_info.Refresh = string(body)
 	account_info.Access_Token, success = insteon.Refresh_Bearer(string(body))
 	fmt.Println("Got refresh " + amazon_id + " and its " + account_info.Access_Token)
-	if Accounts == nil{
-	    Accounts = make(map[string]Acc_Info)
+	if Accounts == nil {
+		Accounts = make(map[string]Acc_Info)
 	}
 	Accounts[amazon_id] = account_info
+	return
+}
+
+type toShorten struct {
+	LongUrl string `json:"longUrl"`
+}
+
+func ShortenURL(long_url string) (shorturl string) {
+	u := "https://www.googleapis.com/urlshortener/v1/url" + "?key=" + secrets.Google_api_key
+	var d toShorten
+	d.LongUrl = long_url
+	data, _ := json.Marshal(d)
+	client := &http.Client{}
+	req, _ := http.NewRequest("POST", u, bytes.NewBuffer(data))
+	req.Header.Add("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("Errored when sending request to the server")
+		return
+	}
+	defer resp.Body.Close()
+	resp_body, _ := ioutil.ReadAll(resp.Body)
+	var i map[string]string
+	json.Unmarshal(resp_body, &i)
+	shorturl = i["id"]
 	return
 }
